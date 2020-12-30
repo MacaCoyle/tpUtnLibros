c2d035b6a1aca29c9f36580b087c55823704e033
Agustin Avila <agualvila@gmail.com>
2020-12-30 18:04:15 -0300

    primer merge main

---
+++ app.js
@@ -0,0 +1,26 @@
+const { urlencoded } = require('body-parser')
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const port = process.env.PORT ? process.env.PORT : 3000
+
+
+var mongoDB = 'mongodb://localhost/whereismybook'
+mongoose.connect(mongoDB, {useNewUrlParser: true})
+mongoose.Promise = global.Promise
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error: '))
+
+//Prueba de servidor.
+app.get('/', (req, res) => {
+    res.send('Estas vivo en el: ' + port)
+})
+
+app.use(urlencoded())
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log('Im alive at port: ' + port)
+})
---
+++ models/libro.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const AutoIncrement = require('mongoose-sequence')(mongoose)
+const Schema = mongoose.Schema
+
+/* De los libros, el nombre, una descripcion, su categoria y la persona a la cual se le ha prestado el libro. Para representar que un libro se encuentra en la biblioteca se puede utilizar cualquiera de las siguientes estrategias: null para libros en la biblioteca en el campo de persona_id, que el usuario se encuentre ingresado como una persona mas. 
+Se solicita verificar que el usuario no envie los campos requeridos solo con espacios en blanco.
+
+Para asegurar los aciertos de las busquedas, se sugiere el guardado en mayusculas de todos los campos alfanumericos y recordar hacer las verificaciones teniendo en cuenta esto.
+*/
+
+const libroSchema = new Schema({
+    nombre: {type: String, required: true},
+    descripcion: {type: String, required: true},
+    categoria : {type: Schema.ObjectId, ref: 'Categoria', required: true },
+    tenedor: {type: Schema.ObjectId, ref: 'Persona'}
+})
+
+libroSchema.methods.toString = function(){
+    return `Libro: ${this.nombre} Descripcion: ${this.descripcion} Categoria: ${this.categoria} Quien lo tiene: ${this.tenedor}`
+}
+
+libroSchema.statics.allLibros = function(cb){
+    return this.find({}, cb)
+}
+
+libroSchema.statics.add = function(aLibro, cb){
+    this.create(aLibro, cb)
+}
+
+libroSchema.methods.updateTenedor = function(nuevoTenedor){
+    this.tenedor = nuevoTenedor
+}
+
+//Plugins para validar elementos unicos y de autoincremento
+categoriaSchema.plugin(AutoIncrement, {inc_field: 'id'})
+
+module.exports = mongoose.model('Libro', libroSchema)
