e16e4e30eef3cc16bc726b603da2c5d0be9dd81e
Agustin Avila <agualvila@gmail.com>
2020-12-30 22:05:12 -0300

    pequeño reordenamiento de app.js

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-const { urlencoded } = require('body-parser')
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const { urlencoded } = require("body-parser");
 
-const app = express()
+const port = process.env.PORT ? process.env.PORT : 3000;
 
-const port = process.env.PORT ? process.env.PORT : 3000
-
-
-var mongoDB = 'mongodb://localhost/whereismybook'
-mongoose.connect(mongoDB, {useNewUrlParser: true})
-mongoose.Promise = global.Promise
-var db = mongoose.connection
-db.on('error', console.error.bind(console, 'MongoDB connection error: '))
+// Conexion con la base de datos MongoDB
+var mongoDB = "mongodb://localhost/whereismybook";
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
 
 //Prueba de servidor.
-app.get('/', (req, res) => {
-    res.send('Estas vivo en el: ' + port)
-})
+app.get("/", (req, res) => {
+  res.send("Estas vivo en el: " + port);
+});
 
-app.use(urlencoded())
+app.use(urlencoded());
 app.use(express.json());
 
+//Comienza aplicacion
+app.use('/api/persona', require('./controllers/personaAPIController')); // API
+
 app.listen(port, () => {
-    console.log('Im alive at port: ' + port)
-})
+  console.log("Im alive at port: " + port);
+});
